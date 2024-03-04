90800f3464f5a925805db7b1c83b6ef6605ef3b7
vytzab <vytautas.zabielskas@gmail.com>
2024-03-04 17:41:02 +0200

    Initial commit

---
+++ src/test/java/com/vytzab/todowebservices/TodoWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vytzab.todowebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
